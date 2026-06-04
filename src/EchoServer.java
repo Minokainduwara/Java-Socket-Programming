e3550406f3b4656207f42ec287d291c5a88fba80
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-04 14:14:22 +0530

    Feat: Server Side Connection

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Waiting for clients...");
+            ServerSocket serverSocket = new ServerSocket(9804);
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connection established!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
