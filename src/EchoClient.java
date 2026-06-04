f40be87eaad2b8fe351944d31d4dba695263fd32
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-04 14:52:48 +0530

    Feat: Send a msg from Client Side

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class EchoClient {
@@ -7,6 +10,14 @@ public class EchoClient {
         try {
             System.out.println("EchoClient started!");
             Socket soc = new Socket("localhost", 9804);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a message to send to the server:");
+            String message = userInput.readLine();
+
+            //Send the String to the User
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println(message);
+            out.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
