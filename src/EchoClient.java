39bdc11484b747af0aab97ec8d484ccdefdf7777
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-04 14:17:01 +0530

    Feat: Client Side Connection

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -1,2 +1,14 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class EchoClient {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("EchoClient started!");
+            Socket soc = new Socket("localhost", 9804);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
