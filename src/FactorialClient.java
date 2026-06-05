d75bc8cd065b8a01cce4efdb48b601678dc234d8
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-05 15:00:59 +0530

    Feat: Calculate Factorial number in Server side

--- src/FactorialClient.java
+++ src/FactorialClient.java
@@ -1,2 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class FactorialClient {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client started!");
+            Socket soc = new Socket("localhost", 9805);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a number to calculate its factorial:");
+            int number = Integer.parseInt(userInput.readLine());
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println(number);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/FactorialServer.java
+++ src/FactorialServer.java
@@ -1,2 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class FactorialServer {
+    public static void main(String[] args) {
+        try {
+            System.out.println("FactorialServer started! Waiting for clients...");
+            ServerSocket serverSocket = new ServerSocket(9805);
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            int number = Integer.parseInt(userInput.readLine());
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.println("Factorial of " + number + " is " + factorial(number));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
 }
